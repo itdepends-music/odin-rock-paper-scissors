75ad7897ab459ace90efad38582d627a7901cf1b
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2022-12-28 10:26:39 +1300

    add playRound()

--- javascript.js
+++ javascript.js
@@ -3,10 +3,32 @@ function getComputerChoice () {
     random_number = Math.floor(Math.random() * 3) + 1;
 
     if (random_number === 1) {
-        return 'Rock';
+        return "Rock";
     } else if (random_number === 2) {
-        return 'Paper';
+        return "Paper";
     } else {
-        return 'Scissors'
+        return "Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    // Change case to first letter uppercase and rest lowercase
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return `Draw! Both players played ${playerSelection}`;
+    }
+
+    let didPlayerWin = (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    );
+
+    if (didPlayerWin) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
